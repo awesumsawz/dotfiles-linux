a1e376ad8a69144dc8ba618d31351c4974f2461f
Jason Biggs <development@jason-biggs.com>
2026-07-10 11:43:06 -0500

    added linux cc alias

--- .config/zsh/linux.zsh
+++ .config/zsh/linux.zsh
@@ -2,6 +2,7 @@ export DOCKER_HOST=unix://$XDG_RUNTIME_DIR/docker.sock
 export PATH="$HOME/.local/bin:/usr/bin:$PATH"
 alias nrs="home-manager switch --flake $HOME/nix-arch#rogue"
 alias logout="hyprctl dispatch exit"
+alias cc="claude --permission-mode auto"
 export GTK_THEME=Dracula
 export XDG_CURRENT_DESKTOP=GNOME
 export XDG_SESSION_TYPE=wayland
